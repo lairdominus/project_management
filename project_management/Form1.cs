4906c2d2a0a6248171cd8aafc05e3bee6fa3626d
lairdominus <domthemaster@yahoo.co.uk>
2015-11-20 10:07:18 +0000

    String message

--- project_management/Form1.cs
+++ project_management/Form1.cs
@@ -19,7 +19,7 @@ namespace project_management
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("FAIL");
+            MessageBox.Show(textBox1.Text);
         }
     }
 }
